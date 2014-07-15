7a3d992e71ce4250e19b5966aec6ac5951bdbada
Romain Michon <rmnmichon@gmail.com>
2014-07-14 21:16:41 -0700

    Adding faust2android

---
+++ src/com/grame/faust/faust.java
@@ -0,0 +1,43 @@
+package com.grame.faust;
+
+import com.grame.faust_dsp.faustObject;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class faust extends Activity {
+    /** Called when the activity is first created. */
+	Thread thread;
+	boolean on = true; // process on/off
+	faustObject faustCpp = new faustObject();
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        faustCpp.initFaust();
+        
+        thread = new Thread() {
+			public void run() {
+				setPriority(Thread.MAX_PRIORITY);
+				faustCpp.startAudio();
+				while(on){
+					faustCpp.processDSP();
+				}
+				faustCpp.startAudio();
+			}
+		};
+		thread.start();   
+    }
+    public void onDestroy(){
+    	super.onDestroy();
+    	on = false;
+    	try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	thread = null;
+    	
+    }
+}
